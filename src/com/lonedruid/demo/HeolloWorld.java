d9473730d9b1947ca35f4e74352cacedd806f5ca
leohaha00 <1509779016@qq.com>
2018-01-07 14:02:56 +0800

    Initial commit

---
+++ src/com/lonedruid/demo/HeolloWorld.java
@@ -0,0 +1,11 @@
+package com.lonedruid.demo;
+
+/**
+ * Created by liwen on 2018-01-07.
+ */
+public class HeolloWorld {
+    public static  void main(String[] args)
+    {
+        System.out.print("hello world");
+    }
+}
